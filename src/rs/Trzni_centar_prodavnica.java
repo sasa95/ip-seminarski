f973c9328eb68ea026130d5cd86b6f493e8e01a9
Saša Drmić <sasadrmic032@gmail.com>
2017-06-09 11:13:24 +0200

    Added class Trzni_centar_prodavnica

---
+++ src/rs/Trzni_centar_prodavnica.java
@@ -0,0 +1,18 @@
+package rs;
+
+public class Trzni_centar_prodavnica {
+	private String nazivProdavnice;
+	private int idTrznogCentra;
+	public String getNazivProdavnice() {
+		return nazivProdavnice;
+	}
+	public void setNazivProdavnice(String nazivProdavnice) {
+		this.nazivProdavnice = nazivProdavnice;
+	}
+	public int getIdTrznogCentra() {
+		return idTrznogCentra;
+	}
+	public void setIdTrznogCentra(int idTrznogCentra) {
+		this.idTrznogCentra = idTrznogCentra;
+	}
+}
